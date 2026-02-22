64675bb469c9895cea9b427fcb3addff4b6bccdd
Sughosh505 <sughosh.magesh@gmail.com>
2026-02-22 16:16:02 +0530

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("master_dataset_final.csv")
-
-# Only check numeric columns
-numeric_df = df.select_dtypes(include="number")
-print(numeric_df.corr()["optimal_model"].sort_values(ascending=False))
